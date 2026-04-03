4bb8956e8e78756a12bd345e4eb2ac2f90332dd2
reyaan-nair <contact.reyaan.nair@gmail.com>
2026-04-03 19:50:01 +1000

    Add files via upload

---
+++ Activity2.py
@@ -0,0 +1,7 @@
+num=int(input("what is the number?" ) )
+if  num  > 10:
+    print("it is greater than 10")
+else:
+    print("it is less than 10")    
+
+
---
+++ Activity3.py
@@ -0,0 +1,9 @@
+# take the age as the input from the user and than using the if and else condition check if the age is greater than 18 or less than 18. if the age is greater than 18 print you can vote else print you canot vote
+age=int(input("what your age? "))
+if age > 18:
+    print("you can vote")
+elif age < 18:
+    print("no you cant vote")
+else:
+    print("you are 18 yes you can vote")    
+
